ba4b2cf5068bf95e76e7c57665ed374c48ec2237
hantiansheng <hantiansheng@huohua.cn>
2019-11-01 16:21:49 +0800

    activiti app ui修改mysql数据源

--- modules/activiti-ui/start.sh
+++ modules/activiti-ui/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Building all submodules"
-mvn -T 1C clean install -DskipTests
+mvn -T 1C clean install -DskipTests --settings /Users/hantiansheng/.m3/settings.xml
 STATUS=$?
 if [ $STATUS -eq 0 ]
 then
@@ -9,7 +9,7 @@ then
 	# Run war
 	echo "Running war file"
 	export MAVEN_OPTS="$MAVEN_OPTS -noverify -Xms512m -Xmx1024m -XX:MaxPermSize=512m -Xdebug -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
-	mvn clean tomcat7:run
+	mvn clean tomcat7:run --settings /Users/hantiansheng/.m3/settings.xml
 else
 	say -v Cellos "Dum dum dum dum dum dum dum he he he ho ho ho fa lah lah lah lah lah lah fa lah full hoo hoo hoo"
     echo "Error while building root pom. Halting."
